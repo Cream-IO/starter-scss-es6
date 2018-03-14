d164bd18bd1023ea06c505d5bca1233265e41706
Quentin Raimbault <quentin.raimbault@hetic.net>
2018-03-14 15:39:28 +0100

    Better documentation in gulpfile (#2)
    #1

--- gulpfile.js
+++ gulpfile.js
@@ -3,20 +3,31 @@
 // Just say we use gulp
 var gulp = require('gulp');
 
-// Some dependencies
+// Used to compile SASS/SCSS files
 var sass = require('gulp-sass');
+// Used to rename files
 var rename = require('gulp-rename');
+// Used to concatenate multiple files in one
 var concat = require('gulp-concat');
+// Used to minify JS
 var uglify = require('gulp-uglify');
-var browserSync = require('browser-sync').create();
+// Used to reindent HTML
 var htmlbeautify = require('gulp-html-beautify');
+// Used to reindent SCSS/SASS & JS
 var jsbeautify = require('gulp-jsbeautifier');
+// Used to auto refresh browser and serve files
+var browserSync = require('browser-sync').create();
 
 
 /**
-  * Building tasks
+  * BUILDING TASKS
 ***/
 
+/**
+  * SCSS BUILDING
+  * Takes src/scss/master.scss and compiles it and all
+  * the imports to public/css/master.min.css
+***/
 gulp.task('scss', function() {
     return gulp.src('src/scss/master.scss')
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
@@ -25,6 +36,11 @@ gulp.task('scss', function() {
         .pipe(browserSync.stream());
 });
 
+/**
+  * JS BUILDING
+  * Takes src/js/**.js, concats and minfiies it
+  * to public/js/app.min.js
+***/
 gulp.task('scripts', function() {
     return gulp.src('src/js/**/*.js')
         .pipe(concat('app.min.js'))
@@ -35,7 +51,12 @@ gulp.task('scripts', function() {
 
 
 /**
-  * Indentation part
+  * INDENTATION TASKS
+***/
+
+/**
+  * Reindents all SCSS files under src/scss
+  * using 2 spaces tabs (standard for CSS)
 ***/
 gulp.task('indentscss', function() {
     return gulp.src('src/scss/**/*.scss', {base: './'})
@@ -45,12 +66,20 @@ gulp.task('indentscss', function() {
         .pipe(gulp.dest('./'));
 });
 
+/**
+  * Reindents all JS files under src/js
+  * using 4 spaces tabs (standard for JS)
+***/
 gulp.task('indentjs', function() {
     return gulp.src('src/js/**/*.js', {base: './'})
         .pipe(jsbeautify())
         .pipe(gulp.dest('./'));
 });
 
+/**
+  * Reindents all HTML files under public
+  * using 2 spaces tabs (standard for HTML)
+***/
 gulp.task('indenthtml', function() {
   var options = {
       "indent_size": 2,
@@ -62,18 +91,32 @@ gulp.task('indenthtml', function() {
 
 
 /**
-  * Main tasks
+  * MAIN TASKS
 ***/
 
+/**
+  * Reindents all JS, SCSS and HTML files
+***/
 gulp.task('indent', ['indentscss', 'indentjs', 'indenthtml']);
 
+/**
+  * Watches all JS & SCSS files and recompiles it when modified
+***/
 gulp.task('watch', function() {
   gulp.watch('src/js/**/*.js', ['scripts']);
   gulp.watch('src/scss/master.scss', ['scss']);
 });
 
+/**
+  * Reindents then build SCSS & JS
+***/
 gulp.task('build', ['indent', 'scss', 'scripts']);
 
+/**
+  * Builds SCSS & JS, watches it
+  * opens a new browser and auto
+  * reloads when files are modified
+***/
 gulp.task('default', ['build', 'watch'], function() {
     browserSync.init({
         server: {
