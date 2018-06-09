93ed8453834eb09e00f70e899ea4203a77cc77d0
Quentin Raimbault <quentin.raimbault@hetic.net>
2018-06-09 13:06:19 +0200

    ✨ Now handling ES6 & JSHint for linting
    Uses Babel for ES6 to ES2015 transpilling & Webpack for ES2015 polyfill

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {});
+document.addEventListener('DOMContentLoaded', function() {
+    'use strict';
+    console.log('Javascript Loaded !');
+});
